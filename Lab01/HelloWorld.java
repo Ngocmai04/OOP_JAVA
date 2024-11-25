aeaa75251640bb3d40fcb9fb9b034c95efe95383
Ngocmai04 <truongngocmai3@gmail.com>
2024-11-25 23:46:21 +0700

    update

--- Lab01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-    public static void main(String args[]){ 
-        System. out.print("Xin chao \n cac ban!");
-        System. out.print("Hello \t world!"); 
-
-    } // end of method main 
-}
