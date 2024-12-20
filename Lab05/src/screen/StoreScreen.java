0a5034e1d191495ddf1b4994d230a45cf008bf2b
Ngocmai04 <truongngocmai3@gmail.com>
2024-12-20 13:11:55 +0700

    origin

---
+++ Lab05/src/screen/StoreScreen.java
@@ -0,0 +1,7 @@
+package screen;
+import javax.swing.JFrame;
+public class StoreScreen extends JFrame{
+    private Store store;
+    
+    
+}
--- Lab05/src/swing/AWTAccumulator.java
+++ Lab05/src/swing/AWTAccumulator.java
@@ -1,3 +1,4 @@
+package swing;
 
 
 import java.awt.*;
